31311876b4772f268f1e31a497f988bd4c3ee1bf
Matilda Mared <matildamared@live.se>
2022-12-28 09:29:15 +0100

    add styling to meal item

--- components/MealItem.js
+++ components/MealItem.js
@@ -2,16 +2,22 @@ import { Text, View, StyleSheet, Pressable, Image } from "react-native";
 
 function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 	return (
-		<View>
-			<Pressable>
+		<View style={styles.container}>
+			<Pressable
+				android_ripple={{ color: "#ccc" }}
+				style={({ pressed }) => [
+					styles.pressable,
+					pressed ? { opacity: 0.75 } : null,
+				]}
+			>
 				<View>
 					<Image style={styles.image} source={{ uri: imageUrl }} />
 					<Text style={styles.title}>{title}</Text>
 				</View>
-				<View>
-					<Text>{duration}min</Text>
-					<Text>{complexity.toUpperCase()}</Text>
-					<Text>{affordability.toUpperCase()}</Text>
+				<View style={styles.details}>
+					<Text style={styles.detailItem}>{duration}min</Text>
+					<Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+					<Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
 				</View>
 			</Pressable>
 		</View>
@@ -19,6 +25,20 @@ function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 }
 
 const styles = StyleSheet.create({
+	container: {
+		margin: 16,
+		borderRadius: 8,
+		backgroundColor: "white",
+		elevation: 2,
+		shadowColor: "black",
+		shadowOpacity: 0.15,
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 8,
+	},
+	pressable: {
+		overflow: "hidden",
+		borderRadius: 8,
+	},
 	image: {
 		width: "100%",
 		height: 200,
@@ -27,6 +47,17 @@ const styles = StyleSheet.create({
 		fontWeight: "bold",
 		textAlign: "center",
 		fontSize: 18,
+		paddingTop: 16,
+		paddingHorizontal: 16,
+	},
+	details: {
+		flexDirection: "row",
+		justifyContent: "center",
+		padding: 16,
+	},
+	detailItem: {
+		fontSize: 12,
+		marginHorizontal: 4,
 	},
 });
 
